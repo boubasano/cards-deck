a79430a60d3e9574b31e1823b0c3e7836897dfcb
boubasano <48012386+boubasano@users.noreply.github.com>
2020-09-14 14:34:22 +0200

    projet card magic

---
+++ config/config.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Config files
+ */
+
+//ini_set('allow_url_include', 1);
+
+const WEBSITE_TITLE = "Vote App";
+const BASE_URL = "/CoursPHPVoteApp";
+
+const CLASSES_SOURCES = [
+    'src/Controller',
+    'config',
+    'src/Entity',
+    'src/Repository',
+    'src/Services',
+];
